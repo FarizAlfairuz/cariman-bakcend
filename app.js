7c5264db8cdb11bdecbeafb7deec82d519e99612
Fariz <farizalfairuz27@gmail.com>
2021-05-24 10:42:45 +0700

    initial push

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+
+app.use('/api', routes);
+
+app.listen(5000, () => {
+    console.log(`Listening to port 5000`);
+});
