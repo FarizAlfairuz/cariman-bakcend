de9035386855ce2326c267296bf005f11a049282
Fariz <farizalfairuz27@gmail.com>
2021-05-25 14:46:35 +0700

    port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
@@ -11,6 +12,6 @@ app.use(cors());
 
 app.use('/api', routes);
 
-app.listen(6000, () => {
-    console.log(`Listening to port 6000`);
+app.listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`);
 });
