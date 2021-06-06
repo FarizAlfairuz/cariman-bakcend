5c89dcdc4df2a1230cc21f83509ebbe79b0f05a9
Fariz <farizalfairuz27@gmail.com>
2021-06-06 07:08:23 +0700

    perbaikan

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -11,6 +11,6 @@ app.use(cors());
 
 app.use('/api', routes);
 
-app.listen(5000, () => {
-    console.log(`Listening to port 5000`);
+app.listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`);
 });
